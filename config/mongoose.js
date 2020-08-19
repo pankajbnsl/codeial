67e0ac19d006ed5805243276d1969b55ed4f3f65
pankajbnsl <luckypnkj21@gmail.com>
2020-08-19 14:45:02 +0530

    first commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/codial_development');
 
 const db = mongoose.connection;
+
+db.on('error', console.log.bind(console, "error connection to mondodb"));
+
+db.once('open',function(){
+    console.log('connected to database::Mongodb');
+});
+
+module.exports = db;
--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+
+// Extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-const expressLayouts = require('express-ejs-layouts');
-app.use(expressLayouts);
-app.use(express.static('./assets'));
+// use express router
+app.use('/', require('./routes'));
+
+
+
+
 app.listen(port, function(err){
     if(err){
         console.log('error in running the express server on the port 8000');
