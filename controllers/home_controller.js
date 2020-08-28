13cd1a63f50737f6cc7216bdf9e389329abab87b
pankajbnsl <luckypnkj21@gmail.com>
2020-08-29 00:56:20 +0530

    create post option and check authentication

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,13 @@
+const Post = require('../models/post');
+
+
 module.exports.home = function(req, res){
-    return res.render('home',{
-      title: "Home"
-    });
+
+     Post.find({}).populate('user').exec(function(err,posts){
+      return res.render('home',{
+        title: "Global Chat | Home",
+        posts: posts
+      });
+     });
+    
 }
