7b3cf134255c85e2cbc6089c831a9316fdcc726f
pankajbnsl <luckypnkj21@gmail.com>
2020-08-31 01:26:45 +0530

    converting code to Async Await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,27 +1,30 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req, res){
-
-     Post.find({})
-     .populate('user')
-     .populate({
-       path:'comments',
-       populate: {
-         path: 'user'
-       }
-     })
-     .exec(function(err,posts){
-
-      User.find({}, function(err, users){
-        return res.render('home',{
-          title: "Global Chat | Home",
-          posts: posts,
-          all_users: users
-        });
+module.exports.home = async function(req, res){
+     
+     try{
+      let posts = await Post.find({})
+      .populate('user')
+      .populate({
+        path:'comments',
+        populate: {
+          path: 'user'
+        }
       });
-
       
-     });
+      let users = await User.find({});
+ 
+       return res.render('home',{
+         title: "Global Chat | Home",
+         posts: posts,
+         all_users: users
+       });
+     }catch(err){
+        console.log('error',err);
+     }
+
+  
+  
     
 }
